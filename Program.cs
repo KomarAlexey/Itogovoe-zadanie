77c6a7eb26b48534a3f3ad957c1d1bc9a26d96fb
Alex <angrylexa@mail.ru>
2022-09-20 19:47:48 +0300

    добавил функцию for

--- Program.cs
+++ Program.cs
@@ -12,3 +12,11 @@ string[] stringArray = new string[4] { "Hello", ":)", "три", "четыре" }
 string[] miniArray = new string[stringArray.Length];
 int letter_counter = 0;
 int size = 3;
+for (int i = 0; i<stringArray.Length; i++)
+{
+    if (stringArray[i].Length <=size)
+    {
+        miniArray[letter_counter]=stringArray[i];  
+        letter_counter++;   
+    }
+}
