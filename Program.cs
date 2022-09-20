81d710b5f777373978b26d229ce35af2e2242c1d
Alex <angrylexa@mail.ru>
2022-09-20 19:44:37 +0300

    написал массив

--- Program.cs
+++ Program.cs
@@ -7,3 +7,6 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
+
+string[] stringArray = new string[4] { "Hello", ":)", "три", "четыре" };
+string[] miniArray = new string[stringArray.Length];
